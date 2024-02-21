e4644663dcc443eab9d6e595f8631e6b4fa3c66e
Fred Clinton Ahanogbe <fred.ahanogbe@gmail.com>
2024-02-21 12:48:52 +0000

    passport

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Controllers;
 
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
