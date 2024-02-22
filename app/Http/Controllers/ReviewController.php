ba6a5df96da96f39e52a911bc6700602f44d16f7
Fred Clinton Ahanogbe <fred.ahanogbe@gmail.com>
2024-02-22 14:53:03 +0000

    reviews

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -79,9 +79,12 @@ class ReviewController extends Controller
      * @param  \App\Models\Model\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateReviewRequest $request, Review $review)
+    public function update(UpdateReviewRequest $request,Product $product, Review $review)
     {
-        //
+        $review->update($request->all());
+        return response([
+            'data' => new ReviewResource($review)
+        ],Response::HTTP_CREATED);
     }
 
     /**
@@ -90,8 +93,10 @@ class ReviewController extends Controller
      * @param  \App\Models\Model\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Review $review)
+    public function destroy(Product $product, Review $review)
     {
-        //
+        $review->delete();
+        return response(null,Response::HTTP_NO_CONTENT);
+        
     }
 }
